717393327058622b6be0f7c06a1de9bcb1aa8e27
ademkilavuz <ademkilavuz@hotmail.com>
2025-07-09 21:39:41 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-et selectedMarker = null;
+let selectedMarker = null;
 const dark_pin_icon = "https://ademkilavuz.github.io/lor-map-app/assets/images/Red_pin.png";
 const default_pin_icon = "https://ademkilavuz.github.io/lor-map-app/assets/images/Yellow_pin.png";
 
